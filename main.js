e7771f52c1568fcd24307a085364d992d7540fdf
adno88 <adno88@o2.pl>
2023-01-19 22:11:28 +0100

    Dodano funkcję wyświetlania linków i numeru telefonu

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const facebook = document.getElementById('facebook');
 const instagram = document.getElementById('instagram');
 const phone = document.getElementById('phone');
 
-
 facebook.addEventListener('click', function() {
-    document.querySelector('.show').innerHTML = '795 777 789';
+    document.querySelector('.show').innerHTML = 'www.facebook.com';
 });
 instagram.addEventListener('click', function() {
-    alert('instagram');
+    document.querySelector('.show').innerHTML = 'www.instagram.com';
 });
 phone.addEventListener('click', function() {
-    alert('phone');
+    document.querySelector('.show').innerHTML = '123 456 789';
 });
